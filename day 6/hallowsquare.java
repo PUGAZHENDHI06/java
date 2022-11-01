3235b62cd73730c18957b4d0611b0e51e9b860d2
Pugazhendhi <112712493+PUGAZHENDHI06@users.noreply.github.com>
2022-11-01 09:05:41 +0530

    Create hallowsquare.java

---
+++ day 6/hallowsquare.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class dopa{
+	private static Scanner sc;
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		System.out.print(" Hollow Square Side = ");
+		int side = sc.nextInt();
+		
+		System.out.println("Print Hollow Square Star Pattern");
+		
+		for (int i = 0; i < side; i++ ) 
+		{
+			for (int j = 0 ; j < side; j++ ) 
+			{
+				if (i == 0 || i == side - 1 || j == 0 || j == side - 1) 
+				{
+					System.out.print("$");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
